45f5757c66380bebdd13568f5d58322edabb4141
lucasestrade <43579718+lucasestrade@users.noreply.github.com>
2021-01-20 17:51:40 +0100

    role add + delete + add role to an user + add rights to a role

--- src/resolvers/Mutations/mutationRole.js
+++ src/resolvers/Mutations/mutationRole.js
@@ -1,8 +1,38 @@
 const { forwardTo } = require('prisma-binding')
-const { getUser } = require('../../utils')
+const { getUser, getUserRightsInGroup } = require('../../utils')
 
 async function createRole (parent, args, ctx, info) {
+    let user = await getUser(ctx);
+    let userId = user.id;
+    let rights = await getUserRightsInGroup(ctx, userId, args.data.group.connect.id);
+
+    // 3 = right able to create a role
+    if(rights.includes("3") || rights.includes("owner")){
+        const role = await ctx.prisma.mutation.createRole(
+            {
+                data: {
+                    name: args.data.name,
+                    rights: {
+                        connect: args.data.rights.connect
+                    },
+                    group: {
+                        connect: {
+                            id: args.data.group.connect.id
+                        } 
+                    }
+                }
+            }
+        )
     
+        return {
+            id: role.id,
+            name: role.name,
+            rights: role.rights,
+            group: role.group
+        }
+    }
+
+    throw new Error("You are not able to create a role in this group");
 }
 
 async function updateRole (parent, args, ctx, info) {
@@ -10,7 +40,22 @@ async function updateRole (parent, args, ctx, info) {
 }
 
 async function deleteRole (parent, args, ctx, info) {
-  
+    let user = await getUser(ctx);
+    let userId = user.id;
+    let rights = await getUserRightsInGroup(ctx, userId, args.data.group.connect.id);
+
+    // 4 = right able to delete a role
+    if(rights.includes("4") || rights.includes("owner")){
+        const roleToDelete = await ctx.prisma.query.role({where:{...args.where}}, info)
+
+        if(roleToDelete === null){
+            throw new notFoundError
+        }
+
+        return forwardTo('prisma')(parent, args, ctx, info);
+    }
+
+    throw new Error("You are not able to delete a role in this group")
 }
 
 module.exports = {
